fa5a2929d0382f0b4255aebc274c8f2be8365693
Anahi Mayancela <anahimayancela@gmail.com>
2025-02-15 14:34:29 -0500

    move form logic out of LoginModal

--- src/app/components/LoginModal.tsx
+++ src/app/components/LoginModal.tsx
@@ -1,53 +1,48 @@
-'use client'
+"use client";
 
-import React, { useState } from 'react';
-import Button from '@mui/material/Button';
-import CustomModal from './CustomModal'; // barrel imports?
-import { Container, TextField, Typography } from '@mui/material';
-import userAuth from '../utils/userAuth';
+import React, { useState } from "react";
+import Button from "@mui/material/Button";
+import CustomModal from "./CustomModal"; // barrel imports?
+import { Container, Typography } from "@mui/material";
+import LoginForm from "./LoginForm";
 
 const LoginModal = () => {
-    const [openModal, setOpenModal] = useState<boolean>(false);
-    const [name, setName] = useState<string>('');
-    const [email, setEmail] = useState<string>('');
+  const [openModal, setOpenModal] = useState<boolean>(false);
 
-    const handleOpen = () => setOpenModal(true);
-    const handleClose = () => setOpenModal(false);
+  const handleOpen = () => setOpenModal(true);
+  const handleClose = () => setOpenModal(false);
 
-    const handleSubmit = (e: any) => { // to-do: add type
-        e.preventDefault();
-
-        userAuth(name, email);
-    };
-
-    console.log(name, email);
-
-    return (
-        <Container>
-            <Button size="large" variant="contained" onClick={handleOpen}>Log in</Button>
-            <CustomModal open={openModal} onClose={handleClose} ariaLabel="Login" ariaDescription="Login modal">
-                <Typography id="modal-title" variant="h6" component="h2"> Login </Typography>
-                <form onSubmit={handleSubmit}> 
-                    <TextField label="Name" variant="standard" defaultValue="Name" onChange={(e) => setName(e.target.value)} />
-                    <TextField label="Email" variant="standard" defaultValue="Email" onChange={(e) => setEmail(e.target.value)} />
-                    <Button type="submit"> Paw-ceed! // Let's go!</Button>
-                </form>
-            </CustomModal>
-        </Container>
-
-    );
+  return (
+    <Container>
+      <Button size="large" variant="contained" onClick={handleOpen}>
+        Log in
+      </Button>
+      <CustomModal
+        open={openModal}
+        onClose={handleClose}
+        ariaLabel="Login"
+        ariaDescription="Login modal"
+      >
+        <Typography id="modal-title" variant="h6" component="h2">
+          Login
+        </Typography>
+        <LoginForm />
+      </CustomModal>
+    </Container>
+  );
 };
 
 export default LoginModal;
 
+// to-do: update styles
 const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 400,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    p: 4,
-  };
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
