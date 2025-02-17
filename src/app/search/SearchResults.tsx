4ff669c798bbf8dc088a2170cc15132d263524ec
Anahi Mayancela <anahimayancela@gmail.com>
2025-02-17 16:27:47 -0500

    add pagination to search results page

--- src/app/search/SearchResults.tsx
+++ src/app/search/SearchResults.tsx
@@ -12,24 +12,41 @@ import BreedSort from "../components/BreedSort";
 import { AgeGroup, SortDirection } from "../utils/types";
 import { SelectChangeEvent } from "@mui/material/Select";
 import AgeSelect from "../components/AgeSelect";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
+const RESULTS_SIZE = 30;
 
 const SearchResults = () => {
   const [breedsSelected, setBreedsSelected] = useState<string[]>([]);
   const [favorites, setFavorites] = useState<string[]>([]);
-  const { data: allDogBreeds, isLoading, error } = useDogBreeds();
   const [sortDirection, setSortDirection] = useState<SortDirection>("asc");
   const [ageGroupSelected, setAgeGroupSelected] = useState<AgeGroup>("all");
+  const [totalResults, setTotalResults] = useState();
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const { data: allDogBreeds, isLoading, error } = useDogBreeds();
 
   const {
     // isLoading: isResultsLoading,
     data: dogDetails,
+    dogIdsData,
     error: searchResultsError,
-  } = useSearchResults(ageGroupSelected, breedsSelected, sortDirection);
+  } = useSearchResults(
+    currentPage,
+    RESULTS_SIZE,
+    ageGroupSelected,
+    breedsSelected,
+    sortDirection
+  );
+
+  console.log(dogDetails, dogIdsData);
 
   const router = useRouter();
 
   const handleBreedSelect = (breeds: string[]) => {
     setBreedsSelected(breeds);
+    setCurrentPage(1);
   };
 
   const handleFavoriteSelect = (id: string, liked: boolean) => {
@@ -44,10 +61,20 @@ const SearchResults = () => {
 
   const handleSortChange = (event: SelectChangeEvent) => {
     setSortDirection(event.target.value as SortDirection); // to-do remove type assertion ?
+    setCurrentPage(1);
   };
 
   const handleAgeGroupSelect = (event: SelectChangeEvent) => {
     setAgeGroupSelected(event.target.value as AgeGroup); // to-do remove type assertion ?
+    setCurrentPage(1);
+  };
+
+  const handlePrev = () => {
+    setCurrentPage(Math.max(currentPage - 1, 0));
+  };
+
+  const handleNext = () => {
+    setCurrentPage(currentPage + 1);
   };
 
   useEffect(() => {
@@ -55,6 +82,10 @@ const SearchResults = () => {
       router.push("/"); //redirect to homepage/login page
     }
   }, [error, router, searchResultsError]);
+
+  useEffect(() => {
+    if (dogIdsData && dogIdsData.total) setTotalResults(dogIdsData.total);
+  }, [dogIdsData]);
 
   if (isLoading) return <Box>Loading breeds...</Box>; // to-do: loading spinner
   //   if (isResultsLoading) return <Box>Loading results...</Box>; // to-do: loading spinner
@@ -76,12 +107,30 @@ const SearchResults = () => {
         onAgeSelectedChange={handleAgeGroupSelect}
       />
       <LogoutButton />
+      {favorites.length > 0 && (
+        <Typography> {favorites.length} liked </Typography>
+      )}
       {dogDetails && (
         <DetailsGrid
           dogs={dogDetails}
           handleFavoriteSelect={handleFavoriteSelect}
         />
       )}
+      <Button onClick={handlePrev} disabled={currentPage === 1}>
+        Prev
+      </Button>
+      <Typography>
+        {currentPage} of{" "}
+        {totalResults && Math.ceil(totalResults / RESULTS_SIZE)}
+      </Typography>
+      <Button
+        onClick={handleNext}
+        disabled={
+          totalResults && currentPage === Math.ceil(totalResults / RESULTS_SIZE)
+        }
+      >
+        Next
+      </Button>
     </>
   );
 };
