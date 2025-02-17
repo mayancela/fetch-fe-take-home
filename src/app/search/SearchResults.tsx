4eadf9d8d770ffb1602ae7c9a046c8f3a5e8dc60
Anahi Mayancela <anahimayancela@gmail.com>
2025-02-17 02:49:34 -0500

    add useSearchResults hooks to SearchResults component

--- src/app/search/SearchResults.tsx
+++ src/app/search/SearchResults.tsx
@@ -6,23 +6,35 @@ import SelectBreeds from "../components/SelectBreeds";
 import Box from "@mui/material/Box";
 import { useRouter } from "next/navigation";
 import LogoutButton from "../components/LogoutButton";
+import useSearchResults from "../hooks/useSearchResults";
 
 const SearchResults = () => {
   const [breedsSelected, setBreedsSelected] = useState<string[]>([]);
   const { data: allDogBreeds, isLoading, error } = useDogBreeds();
+
+  const {
+    // isLoading: isResultsLoading,
+    data: dogDetails,
+    error: searchResultsError,
+  } = useSearchResults(breedsSelected);
+
+  const router = useRouter();
+
+  console.log("dogDetails", dogDetails);
 
   const handleBreedSelect = (breeds: string[]) => {
     setBreedsSelected(breeds);
   };
 
   useEffect(() => {
-    if (error) {
+    if (error || searchResultsError) {
       router.push("/"); //redirect to homepage/login page
     }
-  }, [error, router]);
+  }, [error, router, searchResultsError]);
 
   if (isLoading) return <Box>Loading breeds...</Box>; // to-do: loading spinner
-  if (error) return null; // to-do: loading spinner while redirecting
+  //   if (isResultsLoading) return <Box>Loading results...</Box>; // to-do: loading spinner
+  if (error || searchResultsError) return null; // to-do: loading spinner while redirecting
 
   return (
     <>
