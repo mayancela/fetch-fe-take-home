0e8d9771a61683f98af2a3527b53e29578f068e9
Anahi Mayancela <anahimayancela@gmail.com>
2025-02-17 03:34:20 -0500

    create card for dog details

---
+++ src/app/components/DetailsCard.tsx
@@ -0,0 +1,62 @@
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import React, { useState } from "react";
+import { DogProps } from "../search/page";
+import CardActions from "@mui/material/CardActions";
+import IconButton from "@mui/material/IconButton";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+
+type DetailsCardProps = DogProps & {
+  handleFavoriteSelect: (id: string, liked: boolean) => void;
+};
+const DetailsCard = ({
+  id,
+  age,
+  img,
+  breed,
+  name,
+  zip_code,
+  handleFavoriteSelect,
+}: DetailsCardProps) => {
+  const [isLiked, setIsLiked] = useState(false);
+
+  const handleLiked = () => {
+    setIsLiked(!isLiked);
+    handleFavoriteSelect(id, !isLiked);
+  };
+  return (
+    <Card sx={{ maxWidth: 275, margin: "16px" }} key={`dog-${id}`}>
+      <CardMedia
+        component="img"
+        height="140"
+        width="200"
+        image={img}
+        alt={`image-of-${breed}`}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {name}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Breed: {breed}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Age: {age}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          ZIP Code: {zip_code}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <IconButton onClick={handleLiked}>
+          {!isLiked ? <FavoriteBorderIcon /> : <FavoriteIcon color="primary" />}
+        </IconButton>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default DetailsCard;
