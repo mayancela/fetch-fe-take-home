a4c909c237f500ee2a658c1f8eabab8a79706912
Anahi Mayancela <anahimayancela@gmail.com>
2025-02-25 01:08:57 -0500

    adding loading and error states to search results

--- src/app/search/SearchResults.tsx
+++ src/app/search/SearchResults.tsx
@@ -9,29 +9,36 @@ import LogoutButton from "../components/LogoutButton";
 import useSearchResults from "../hooks/useSearchResults";
 import DetailsGrid from "../components/DetailsGrid";
 import BreedSort from "../components/BreedSort";
-import { AgeGroup, SortDirection } from "../utils/types";
+import { AgeGroup, SortDirectionOptions } from "../utils/types";
 import { SelectChangeEvent } from "@mui/material/Select";
-import AgeSelect from "../components/AgeSelect";
+import AgeSelect from "../components/SelectAge";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import { HOME_PAGE, RESULTS_SIZE } from "../../../config";
 import Container from "@mui/material/Container";
+import { CircularProgress } from "@mui/material";
+import ErrorMessage from "../components/ErrorMessage";
 
 const SearchResults = () => {
   const [breedsSelected, setBreedsSelected] = useState<string[]>([]);
   const [favorites, setFavorites] = useState<string[]>([]);
-  const [sortDirection, setSortDirection] = useState<SortDirection>("asc");
+  const [sortDirection, setSortDirection] =
+    useState<SortDirectionOptions>("asc");
   const [ageGroupSelected, setAgeGroupSelected] = useState<AgeGroup>("all");
   const [totalResults, setTotalResults] = useState();
   const [currentPage, setCurrentPage] = useState(1);
 
-  const { data: allDogBreeds, isLoading, error } = useDogBreeds();
+  const {
+    data: allDogBreeds,
+    isLoading: isDogBreedsLoading,
+    error: dogBreedsError,
+  } = useDogBreeds();
 
   const {
-    // isLoading: isResultsLoading,
+    isLoading: isDogDetailsLoading,
     data: dogDetails,
     dogIdsData,
-    error: searchResultsError,
+    error: dogDetailsError,
   } = useSearchResults(
     currentPage,
     RESULTS_SIZE,
@@ -39,8 +46,6 @@ const SearchResults = () => {
     breedsSelected,
     sortDirection
   );
-
-  console.log(dogDetails, dogIdsData);
 
   const router = useRouter();
 
@@ -60,12 +65,12 @@ const SearchResults = () => {
   };
 
   const handleSortChange = (event: SelectChangeEvent) => {
-    setSortDirection(event.target.value as SortDirection); // to-do remove type assertion ?
+    setSortDirection(event.target.value as SortDirectionOptions);
     setCurrentPage(1);
   };
 
   const handleAgeGroupSelect = (event: SelectChangeEvent) => {
-    setAgeGroupSelected(event.target.value as AgeGroup); // to-do remove type assertion ?
+    setAgeGroupSelected(event.target.value as AgeGroup);
     setCurrentPage(1);
   };
 
@@ -78,84 +83,110 @@ const SearchResults = () => {
   };
 
   const handleMatchRedirect = () => {
-    // useContext?
-
-    router.push(`/match?ids=${favorites.join(",")}`); // update path
+    router.push(`/match?ids=${favorites.join(",")}`);
   };
 
   useEffect(() => {
-    if (error || searchResultsError) {
-      router.push(HOME_PAGE); //redirect to homepage/login page
+    if (dogBreedsError || dogDetailsError) {
+      router.push(HOME_PAGE);
     }
-  }, [error, router, searchResultsError]);
+  }, [dogBreedsError, router, dogDetailsError]);
 
   useEffect(() => {
     if (dogIdsData && dogIdsData.total) setTotalResults(dogIdsData.total);
   }, [dogIdsData]);
 
-  if (isLoading) return <Box>Loading breeds...</Box>; // to-do: loading spinner
-  //   if (isResultsLoading) return <Box>Loading results...</Box>; // to-do: loading spinner
-  if (error || searchResultsError) return null; // to-do: loading spinner while redirecting
+  if (dogBreedsError || dogDetailsError)
+    return <ErrorMessage message={dogBreedsError || dogDetailsError} />;
 
   return (
-    <Container sx={{display: 'flex', flexDirection: 'column'}}>
-      <Typography variant="h1" sx={{fontWeight: '600', textAlign: 'center'}}> Choose Your Favorites</Typography>
-      <Box sx={{mt: 2, mb: 2, display: 'flex', flexDirection: 'row', gap: '10px', justifyContent: 'space-between'}}> 
-        <Box sx={{display: 'flex', gap: '15px'}}> 
-          <SelectBreeds
-            allBreeds={allDogBreeds}
-            selectedBreeds={breedsSelected}
-            onBreedSelect={handleBreedSelect}
-          />
-          <BreedSort
-            sortDirection={sortDirection}
-            onSortChange={handleSortChange}
-          />
-          <AgeSelect
-            ageSelected={ageGroupSelected}
-            onAgeSelectedChange={handleAgeGroupSelect}
-          />
-
-        </Box>
-
-      <Box sx={{display: 'flex', alignItems: 'baseline', gap: "10px"}}> 
-      {favorites.length > 0 && (
-        <Typography> {favorites.length} liked </Typography>
-      )}
-          <Button onClick={handleMatchRedirect} disabled={!favorites.length} size="large">
-            Match
-          </Button>
-      <LogoutButton />
-
-        </Box>
-      </Box>
-
-      {dogDetails && (
-        <DetailsGrid
-          dogs={dogDetails}
-          handleFavoriteSelect={handleFavoriteSelect}
-        />
-      )}
-
-      <Box sx={{display: 'flex', justifyContent: 'center', gap: '15px', mt: 5, mb: 5}}> 
-      <Button onClick={handlePrev} disabled={currentPage === 1} size="small">
-        Prev
-      </Button>
-      <Typography>
-        {currentPage} of {totalResults && Math.ceil(totalResults / RESULTS_SIZE)}
+    <Container sx={{ display: "flex", flexDirection: "column" }}>
+      <Typography variant="h1" sx={{ fontWeight: "600", textAlign: "center" }}>
+        Choose Your Favorites
       </Typography>
-      <Button
-        onClick={handleNext}
-        disabled={
-          totalResults && currentPage === Math.ceil(totalResults / RESULTS_SIZE)
-        }
-        size="small"
-      >
-        Next
-      </Button>
-      </Box>
-      
+      {isDogDetailsLoading || isDogBreedsLoading ? (
+        <CircularProgress />
+      ) : (
+        <>
+          <Box
+            sx={{
+              mt: 2,
+              mb: 2,
+              display: "flex",
+              flexDirection: "row",
+              gap: "10px",
+              justifyContent: "space-between",
+            }}
+          >
+            <Box sx={{ display: "flex", gap: "15px" }}>
+              <SelectBreeds
+                allBreeds={allDogBreeds}
+                selectedBreeds={breedsSelected}
+                onBreedSelect={handleBreedSelect}
+              />
+              <BreedSort
+                sortDirection={sortDirection}
+                onSortChange={handleSortChange}
+              />
+              <AgeSelect
+                ageSelected={ageGroupSelected}
+                onAgeSelectedChange={handleAgeGroupSelect}
+              />
+            </Box>
 
+            <Box sx={{ display: "flex", alignItems: "baseline", gap: "10px" }}>
+              {favorites.length > 0 && (
+                <Typography> {favorites.length} liked </Typography>
+              )}
+              <Button
+                onClick={handleMatchRedirect}
+                disabled={!favorites.length}
+                size="large"
+              >
+                Match
+              </Button>
+              <LogoutButton />
+            </Box>
+          </Box>
+          {dogDetails && (
+            <DetailsGrid
+              dogs={dogDetails}
+              handleFavoriteSelect={handleFavoriteSelect}
+            />
+          )}
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              gap: "15px",
+              mt: 5,
+              mb: 5,
+            }}
+          >
+            <Button
+              onClick={handlePrev}
+              disabled={currentPage === 1}
+              size="small"
+            >
+              Prev
+            </Button>
+            <Typography>
+              {currentPage} of
+              {totalResults && Math.ceil(totalResults / RESULTS_SIZE)}
+            </Typography>
+            <Button
+              onClick={handleNext}
+              disabled={
+                totalResults &&
+                currentPage === Math.ceil(totalResults / RESULTS_SIZE)
+              }
+              size="small"
+            >
+              Next
+            </Button>
+          </Box>
+        </>
+      )}
     </Container>
   );
 };
