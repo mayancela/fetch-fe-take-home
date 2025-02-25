7bc977e8628311042db605c37ad2ab18846b4fc2
Anahi Mayancela <anahimayancela@gmail.com>
2025-02-25 01:36:10 -0500

    add suspense

--- src/app/match/page.tsx
+++ src/app/match/page.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import MatchResults from "./MatchResults";
-import styles from "./page.module.css"
+import styles from "./page.module.css";
 import Container from "@mui/material/Container";
+import { Suspense } from "react";
+import CircularProgress from "@mui/material/CircularProgress";
 
 const MatchPage = () => {
   return (
-    <Container className={styles.page}> 
-    <MatchResults />
-    </Container>
+    <Suspense fallback={<CircularProgress />}>
+      <Container className={styles.page}>
+        <MatchResults />
+      </Container>
+    </Suspense>
   );
 };
 
