ad49c67e20af4668bd8ad4842705f9b232de183f
anahi <anahimayancela@gmail.com>
2025-02-24 23:48:45 -0500

    add box to homepage - merge

--- src/app/Homepage.tsx
+++ src/app/Homepage.tsx
@@ -1,18 +1,17 @@
 import React from "react";
 import Typography from "@mui/material/Typography";
 import LoginFormDialog from "./components/LoginFormDialog";
+import Box from "@mui/material/Box";
 
-// React.FC
 const Homepage = () => {
   return (
-    // replace div w/ box or container?
-    <div>
+    <Box sx={{width: '50%', display: 'flex', flexDirection: 'column', rowGap: '2em'}}>
       <Typography variant="h3" component="h1">
         Your future best friend is just a search away. Let’s find them! 🐾
       </Typography>
       {/* todo: add auth logic if already logged in? */}
       <LoginFormDialog />
-    </div>
+    </Box>
   );
 };
 
