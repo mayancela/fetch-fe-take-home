242e3788ed0180516f33f1b18cb79c12859e40ee
Anahi Mayancela <anahimayancela@gmail.com>
2025-02-25 01:05:54 -0500

    add domain to images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["frontend-take-home.fetch.com"],
+  },
 };
 
 export default nextConfig;
