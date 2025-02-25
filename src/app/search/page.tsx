6a656e4d2f371d8cd0d0f396d0ca95051a767c4f
Anahi Mayancela <anahimayancela@gmail.com>
2025-02-25 01:08:27 -0500

    moving types location

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,23 +1,14 @@
 import React from "react";
 import SearchResults from "./SearchResults";
 import Container from "@mui/material/Container";
-import styles from "./page.module.css"
-
-export type DogProps = {
-  id: string;
-  img: string;
-  name: string;
-  age: number;
-  zip_code: string;
-  breed: string;
-};
+import styles from "./page.module.css";
 
 const Search = () => {
   return (
-    <Container className={styles.page}> 
+    <Container className={styles.page}>
       <SearchResults />
     </Container>
-  )
+  );
 };
 
 export default Search;
--- src/app/utils/types.ts
+++ src/app/utils/types.ts
@@ -1,3 +1,12 @@
-export type SortDirection = "asc" | "desc";
+export type SortDirectionOptions = "asc" | "desc";
 
 export type AgeGroup = "all" | "puppy" | "adult" | "senior";
+
+export type DogProps = {
+  id: string;
+  img: string;
+  name: string;
+  age: number;
+  zip_code: string;
+  breed: string;
+};
