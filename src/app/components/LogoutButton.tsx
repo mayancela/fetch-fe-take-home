66cf91ceeb262c5e7e082ac4232099ee67c8ebda
Anahi Mayancela <anahimayancela@gmail.com>
2025-02-25 01:09:26 -0500

    Adding error message to logout button

--- src/app/components/LogoutButton.tsx
+++ src/app/components/LogoutButton.tsx
@@ -3,6 +3,7 @@ import Button from "@mui/material/Button";
 import { useRouter } from "next/navigation";
 import { HOME_PAGE } from "../../../config";
 import useUserAuth from "@/app/hooks/useUserAuth";
+import ErrorMessage from "./ErrorMessage";
 
 const LogoutButton = () => {
   const { logoutUser, isAuthenticated, isLoading, error } = useUserAuth();
@@ -16,14 +17,17 @@ const LogoutButton = () => {
         router.push(HOME_PAGE);
       }
     } catch {
-      console.error("Logout failed. Please try again"); // to - do update error handling
+      console.error("Error. Please try again.");
     }
   };
 
   return (
-    <Button disabled={!!isLoading} onClick={handleOnClick} size="large">
-      Logout
-    </Button>
+    <>
+      <Button disabled={!!isLoading} onClick={handleOnClick} size="large">
+        Logout
+      </Button>
+      {error && <ErrorMessage message="Logout failed. Please try again." />}
+    </>
   );
 };
 
