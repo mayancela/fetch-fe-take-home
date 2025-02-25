46840bdde63ed6f7f0c967e506a000deafe068c7
anahi <anahimayancela@gmail.com>
2025-02-24 23:32:53 -0500

    adding values to config

--- config.ts
+++ config.ts
@@ -1,4 +1,10 @@
 export const BREED_API_ENDPOINT = "/breeds";
-export const RESULTS_SIZE = 30;
+export const RESULTS_SIZE = 44;
 export const HOME_PAGE = "/"; // change to HOMEPAGE_ROUTE or HOMEPAGE_PATH?
 export const BASE_URL = "https://frontend-take-home-service.fetch.com";
+
+export const LARGE_GAP  = "15px";
+export const MEDIUM_GAP  = "10px";
+export const SMALL_GAP  = "5px";
+
+
