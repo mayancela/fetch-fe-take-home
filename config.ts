734f2b526d393422b1cf315a49f8e5d8068b90e4
Anahi Mayancela <anahimayancela@gmail.com>
2025-02-25 02:13:29 -0500

    styling updates

--- config.ts
+++ config.ts
@@ -1,10 +1,7 @@
-export const BREED_API_ENDPOINT = "/breeds";
 export const RESULTS_SIZE = 44;
-export const HOME_PAGE = "/"; // change to HOMEPAGE_ROUTE or HOMEPAGE_PATH?
+export const HOME_PAGE = "/";
 export const BASE_URL = "https://frontend-take-home-service.fetch.com";
 
-export const LARGE_GAP  = "15px";
-export const MEDIUM_GAP  = "10px";
-export const SMALL_GAP  = "5px";
-
-
+export const LARGE_GAP = "15px";
+export const MEDIUM_GAP = "10px";
+export const SMALL_GAP = "5px";
