6f9623680aaafeaa460e90f727c419cd6ed04b81
Anahi Mayancela <anahimayancela@gmail.com>
2025-02-25 01:09:07 -0500

    adding loading and error states to match results

--- src/app/match/MatchResults.tsx
+++ src/app/match/MatchResults.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Container from "@mui/material/Container";
 import { useRouter, useSearchParams } from "next/navigation";
 import React, { useEffect, useState } from "react";
 import useDogDetails from "../hooks/useDogDetails";
@@ -8,22 +7,22 @@ import fetchDogMatch from "../utils/fetchDogMatch";
 import LogoutButton from "../components/LogoutButton";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
-import { Typography } from "@mui/material";
 import { LARGE_GAP } from "../../../config";
+import Image from "next/image";
+import ErrorMessage from "../components/ErrorMessage";
+import CircularProgress from "@mui/material/CircularProgress";
+import Typography from "@mui/material/Typography";
 
 const MatchResults = () => {
   const [favoriteIds, setFavoriteIds] = useState<string[]>([]);
   const searchParams = useSearchParams();
   const [matchId, setMatchId] = useState("");
   const [isMatchLoading, setIsMatchLoading] = useState(false);
-  const [isMatchError, setMatchError] = useState("");
+  const [matchErrorMessage, setMatchErrorMessage] = useState("");
   const { data, error, isLoading } = useDogDetails([matchId]);
   const [dogData] = data ?? [];
 
   const router = useRouter();
-
-  console.log("match data", data);
-  console.log("favoriteIds", favoriteIds);
 
   useEffect(() => {
     const ids = searchParams.get("ids");
@@ -32,17 +31,20 @@ const MatchResults = () => {
     }
   }, [searchParams]);
 
-  // Fetch match when favoriteIds changes
   useEffect(() => {
     const fetchMatch = async () => {
       setIsMatchLoading(true);
-      setMatchError("");
+      setMatchErrorMessage("");
 
       try {
         const res = await fetchDogMatch("/dogs/match", favoriteIds);
         setMatchId(res.match);
       } catch (error) {
-        setMatchError("An error occurred");
+        if (error instanceof Error) {
+          setMatchErrorMessage(error.message);
+        } else {
+          setMatchErrorMessage("An unexpected error occurred");
+        }
       } finally {
         setIsMatchLoading(false);
       }
@@ -53,31 +55,56 @@ const MatchResults = () => {
     }
   }, [favoriteIds]);
 
+  if (matchErrorMessage || error) <ErrorMessage message={matchErrorMessage} />;
+
+  console.log(dogData);
+
   return (
-    <>
-      {data && (
-        <Box minWidth={500} sx={{display: "flex", flexDirection: 'column', alignItems: 'center'}}>
-          <Typography variant="h1" component="h1"> You have a match! </Typography>
-          <img
+    <Box
+      minWidth={500}
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      {isMatchLoading || (isLoading && <CircularProgress />)}
+      {data && dogData && (
+        <>
+          <Typography variant="h1">You have a match!</Typography>
+          {dogData.img && (
+            <Image
               src={dogData.img}
               alt={`image-of-dog`}
               loading="lazy"
               width={400}
               height={400}
-              style={{borderRadius: '15%'}}
-          />
-          <Box maxWidth={500} sx={{ mt: 2}}>
-            <Typography variant="body1" > 
-              Meet {dogData.name}, a {dogData.breed} who is ready to meet their forever paw-parents. {dogData.name} is {dogData.age} years old and is located at {dogData.zip_code}
+              style={{ borderRadius: "15%", objectFit: "cover" }}
+            />
+          )}
+          <Box maxWidth={500} sx={{ mt: 2 }}>
+            <Typography variant="body1">
+              Meet {dogData.name}, a {dogData.breed} who is ready to meet their
+              forever paw-parents. {dogData.name} is {dogData.age} years old and
+              is located at zipcode {dogData.zip_code}.
             </Typography>
           </Box>
-        </Box>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              gap: LARGE_GAP,
+              mt: 2,
+            }}
+          >
+            <Button onClick={() => router.push("/search")} size="large">
+              Start Over
+            </Button>
+            <LogoutButton />
+          </Box>
+        </>
       )}
-      <Box sx={{display: 'flex', flexDirection: 'row', gap: LARGE_GAP, mt: 2}}>
-        <Button onClick={() => router.push("/search")} size="large"> Start Over </Button>
-        <LogoutButton />
-      </Box>
-    </>
+    </Box>
   );
 };
 
