575334efe4c2bb98c91cbca741d0d6e4b6d6e729
Anahi Mayancela <anahimayancela@gmail.com>
2025-02-25 00:26:55 -0500

    rename and import updates

--- src/app/components/SelectAge.tsx
+++ src/app/components/SelectAge.tsx
@@ -1,18 +1,19 @@
-import { Box, MenuItem } from "@mui/material";
 import FormControl from "@mui/material/FormControl";
 import InputLabel from "@mui/material/InputLabel";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
 import React from "react";
 import { AgeGroup } from "../utils/types";
+import MenuItem from "@mui/material/MenuItem";
+import Box from "@mui/material/Box";
 
 type AgeSelectProps = {
   ageSelected: AgeGroup;
   onAgeSelectedChange: (event: SelectChangeEvent) => void;
 };
-const AgeSelect = ({
+const AgeSelect: React.FC<AgeSelectProps> = ({
   ageSelected = "all",
   onAgeSelectedChange,
-}: AgeSelectProps) => {
+}) => {
   return (
     <Box>
       <FormControl>
